191bcb8cc8c4d57ddde8ae8266808f69e76c18f7
ckhatri <chirag.k.khatri@gmail.com>
2015-07-16 18:41:30 -0500

    Started on top bar which will host logo, along with social icons

--- wp-content/themes/html5blank/index.php
+++ wp-content/themes/html5blank/index.php
@@ -1,17 +1,26 @@
 <?php get_header(); ?>
-	<main role="main">
+<header>
+	<div class = "container-fluid">
+		<div class="row">
+			<div class="topBarHolder">
+				Hello
+			</div>
+		</div>
+	</div>
+</header>
+<!-- 	<main role="main">
 		<!-- section -->
 		<section>
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+			<!-- <h1><?php // _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+			<?php // get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
+			<?php // get_template_part('pagination'); ?> -->
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 
-<?php get_footer(); ?>
+<?php //get_footer(); ?>
